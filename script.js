4e5f1cb2b5c181e462c6db3ffd79e0a6febd562f
karankraina <karanraina1996@gmail.com>
2018-09-30 17:50:13 +0530

    1

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+    $("#btn1").on("click", function () {
+        //alert("click working")
+        // $("#btn1").removeClass("bounceIn").addClass("rollOut").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function (e) {
+            window.location.href = "learning/main.html"
+        // });
+    });
+    $("#btn2").on("click", function () {
+        //alert("click working")
+        // $("#btn1").removeClass("bounceIn").addClass("rollOut").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function (e) {
+            window.location.href = "lalded/index.html"
+        // });
+    });
+    $("#btn3").on("click", function () {
+        //alert("click working")
+        // $("#btn1").removeClass("bounceIn").addClass("rollOut").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function (e) {
+            window.location.href = "translate.html"
+        // });
+    });
+    $("#btn4").on("click", function () {
+        //alert("click working")
+        // $("#btn1").removeClass("bounceIn").addClass("rollOut").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function (e) {
+            window.location.href = "aboutus.html"
+        });
+    // });
+});
